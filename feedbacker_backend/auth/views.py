cd8d57d46fab6ee6316e63999cccd0b5be0dbc6b
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-03-10 22:12:23 -0300

    create register function

--- feedbacker_backend/auth/views.py
+++ feedbacker_backend/auth/views.py
@@ -6,6 +6,8 @@ from rest_framework.authtoken import Token
 from users_handler.models import User;
 from users_handler.serializers import UserSerializer
 
+from django.contrib.auth.hashers import make_password
+
 def get_token(user):
     return Token.objects.create(user=user)
     
@@ -17,3 +19,13 @@ def login(request):
         token = get_token(user)
         return Response({'token': token.key}, status=status.HTTP_200_OK)
     return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+    serializer = UserSerializer(data=data)
+    if serializer.is_valid():
+        password = data['password']
+        serializer.save(password=make_password(password))
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
