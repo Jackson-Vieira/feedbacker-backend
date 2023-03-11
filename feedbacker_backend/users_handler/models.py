aac3d8592a7e10ef5afa5529cde2747dcd518f5f
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-03-10 21:30:34 -0300

    create custom user model

--- feedbacker_backend/users_handler/models.py
+++ feedbacker_backend/users_handler/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from uuid import uuid4;
+
+class User(AbstractUser):
+  apiKey = models.UUIDField(default=uuid4, editable=False, unique=True);
+  created_at = models.DateTimeField(auto_now_add=True);
+  updated_at = models.DateTimeField(auto_now=True);
