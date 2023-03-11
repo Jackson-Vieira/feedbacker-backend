199ac2c46f0cf241e2a4925593fe979f185d0fe7
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-03-11 13:53:11 -0300

    fix register view in auth molule

--- feedbacker_backend/feedbacker_auth/views.py
+++ feedbacker_backend/feedbacker_auth/views.py
@@ -3,8 +3,9 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
 
+
+from users_handler.models import User
 from users_handler.serializers import UserSerializer
 
 
@@ -16,7 +17,8 @@ def register(request):
     if not User.objects.filter(email=email).exists():
         serializer.is_valid(raise_exception=True)
         password = data.get('password')
-        serializer.save(password=make_password(password))
-
+        user = serializer.save(password=make_password(password))
+        user.create_api_key()
+        
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response({'message': 'A User with this email already exists'}, status=status.HTTP_400_BAD_REQUEST)
